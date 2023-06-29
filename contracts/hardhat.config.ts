afb837d1a1068d89aeff692840290a20263a3a5f
BitNet <134201208+bitnet@users.noreply.github.com>
2023-06-29 18:01:29 +0800

    chore: update hardhat.config.ts

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -5,7 +5,7 @@ const config: HardhatUserConfig = {
   solidity: "0.8.18",
   networks: {
     localhost: {
-      chainId: 2031,
+      chainId: 2032,
       url: "http://127.0.0.1:8545",
       accounts: [
         "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"
